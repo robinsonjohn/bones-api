7f229ccb763f7a86c45a37700a8e75f44141f039
John Robinson <john@bayfrontmedia.com>
2021-02-09 18:09:29 -0500

    Added Groups

---
+++ app/Controllers/v1/Groups.php
@@ -0,0 +1,637 @@
+<?php
+
+/**
+ * @package bones-api
+ * @link https://github.com/bayfrontmedia/bones-api
+ * @author John Robinson <john@bayfrontmedia.com>
+ * @copyright 2021 Bayfront Media
+ */
+
+namespace App\Controllers\v1;
+
+use App\Schemas\GroupCollection;
+use App\Schemas\GroupResource;
+use Bayfront\ArrayHelpers\Arr;
+use Bayfront\ArraySchema\InvalidSchemaException;
+use Bayfront\Bones\Exceptions\ControllerException;
+use Bayfront\Bones\Exceptions\HttpException;
+use Bayfront\Bones\Exceptions\ServiceException;
+use Bayfront\Container\NotFoundException;
+use Bayfront\HttpRequest\Request;
+use Bayfront\HttpResponse\InvalidStatusCodeException;
+use Bayfront\LeakyBucket\AdapterException;
+use Bayfront\LeakyBucket\BucketException;
+use Bayfront\MonologFactory\Exceptions\ChannelNotFoundException;
+use Bayfront\PDO\Exceptions\QueryException;
+use Bayfront\RBAC\Exceptions\InvalidGroupException;
+use Bayfront\RBAC\Exceptions\InvalidKeysException;
+use Bayfront\RBAC\Exceptions\NameExistsException;
+use Bayfront\Validator\Validate;
+use Bayfront\Validator\ValidationException;
+use PDOException;
+
+/**
+ * Groups controller.
+ *
+ * This controller allows rate limited authenticated access to endpoints.
+ */
+class Groups extends ApiController
+{
+
+    /**
+     * Groups constructor.
+     *
+     * @throws ControllerException
+     * @throws HttpException
+     * @throws InvalidStatusCodeException
+     * @throws NotFoundException
+     * @throws ServiceException
+     * @throws AdapterException
+     * @throws BucketException
+     */
+
+    public function __construct()
+    {
+        parent::__construct(true);
+    }
+
+    /**
+     * Create new group.
+     *
+     * @return void
+     *
+     * @throws ChannelNotFoundException
+     * @throws HttpException
+     * @throws InvalidGroupException
+     * @throws InvalidSchemaException
+     * @throws InvalidStatusCodeException
+     * @throws NotFoundException
+     */
+
+    protected function _createGroup(): void
+    {
+
+        /*
+         * Check permissions
+         */
+
+        if (!$this->hasPermissions('global.groups.create')) {
+
+            abort(403, 'Unable to create group: insufficient permissions');
+            die;
+
+        }
+
+        /*
+         * Get body
+         */
+
+        $body = $this->api->getBody([
+            'name'
+        ]); // Required keys
+
+        if (!empty(Arr::except($body, [ // If invalid members have been sent
+            'name'
+        ]))) {
+
+            abort(400, 'Unable to create group: request body contains invalid members');
+            die;
+
+        }
+
+        /*
+         * Validate body
+         */
+
+        try {
+
+            Validate::as($body, [
+                'name' => 'string'
+            ]);
+
+        } catch (ValidationException $e) {
+
+            abort(400, $e->getMessage());
+            die;
+
+        }
+
+        /*
+         * Perform action
+         */
+
+        try {
+
+            $id = $this->auth->createGroup($body);
+
+        } catch (InvalidKeysException $e) {
+
+            abort(400, 'Unable to create group: invalid members');
+            die;
+
+        } catch (NameExistsException $e) {
+
+            abort(409, 'Unable to create group: name already exists');
+            die;
+
+        }
+
+        /*
+         * Log action
+         */
+
+        log_info('Group created', [
+            'id' => $id
+        ]);
+
+        /*
+         * Do event
+         */
+
+        do_event('group.create', $id);
+
+        /*
+         * Build schema
+         */
+
+        $schema = GroupResource::create($this->auth->getGroup($id), [
+            'object_prefix' => '/groups'
+        ]);
+
+        /*
+         * Send response
+         */
+
+        $this->response->sendJson($schema);
+
+    }
+
+    /**
+     * Update group.
+     *
+     * @param string $id
+     *
+     * @return void
+     *
+     * @throws ChannelNotFoundException
+     * @throws HttpException
+     * @throws InvalidGroupException
+     * @throws InvalidSchemaException
+     * @throws InvalidStatusCodeException
+     * @throws NotFoundException
+     */
+
+    protected function _updateGroup(string $id): void
+    {
+
+        /*
+         * Check permissions
+         */
+
+        if (!$this->hasPermissions('global.groups.update')) {
+
+            abort(403, 'Unable to update group: insufficient permissions');
+            die;
+
+        }
+
+        /*
+         * Get body
+         */
+
+        $body = $this->api->getBody();
+
+        if (!empty(Arr::except($body, [ // If invalid members have been sent
+            'name'
+        ]))) {
+
+            abort(400, 'Unable to update group: request body contains invalid members');
+            die;
+
+        }
+
+        /*
+         * Validate body
+         */
+
+        try {
+
+            Validate::as($body, [
+                'name' => 'string'
+            ]);
+
+        } catch (ValidationException $e) {
+
+            abort(400, $e->getMessage());
+            die;
+
+        }
+
+        /*
+         * Perform action
+         */
+
+        try {
+
+            $this->auth->updateGroup($id, $body);
+
+        } catch (InvalidKeysException $e) {
+
+            abort(400, 'Unable to update group: invalid members');
+            die;
+
+        } catch (InvalidGroupException $e) {
+
+            abort(404, 'Unable to update group: group ID does not exist');
+            die;
+
+        } catch (NameExistsException $e) {
+
+            abort(409, 'Unable to update group: name already exists');
+            die;
+
+        }
+
+        /*
+         * Log action
+         */
+
+        log_info('Group updated', [
+            'id' => $id
+        ]);
+
+        /*
+         * Do event
+         */
+
+        do_event('group.update', $id);
+
+        /*
+         * Build schema
+         */
+
+        $schema = GroupResource::create($this->auth->getGroup($id), [
+            'object_prefix' => '/groups'
+        ]);
+
+        /*
+         * Send response
+         */
+
+        $this->response->sendJson($schema);
+
+    }
+
+    /**
+     * Get single group.
+     *
+     * @param string $id
+     *
+     * @return void
+     *
+     * @throws HttpException
+     * @throws InvalidSchemaException
+     * @throws InvalidStatusCodeException
+     * @throws NotFoundException
+     */
+
+    protected function _getGroup(string $id): void
+    {
+
+        /*
+         * Check permissions
+         */
+
+        if (!$this->hasAnyPermissions([
+                'global.groups.read',
+                'self.groups.read'
+            ]) || (!$this->hasPermissions('global.groups.read')
+                && !in_array($id, $this->user_groups))) {
+
+            abort(403, 'Unable to get group: insufficient permissions');
+            die;
+
+        }
+
+        /*
+         * Get request
+         */
+
+        $request = $this->api->parseQuery(
+            Request::getQuery(),
+            Arr::get(Request::getQuery(), 'page.size', get_config('api.default_page_size', 10)),
+            get_config('api.max_page_size', 100)
+        );
+
+        /*
+         * Validate field types and fields
+         *
+         * Valid fields should match what is available to be
+         * returned in the schema.
+         */
+
+        if (!empty(Arr::except($request['fields'], [ // Valid field types
+                'groups'
+            ])) || !empty(Arr::except(array_flip(Arr::get($request['fields'], 'groups', [])), [ // Valid fields
+                'id',
+                'name',
+                'createdAt',
+                'updatedAt'
+            ]))) {
+
+            abort(400, 'Unable to get group: query string contains invalid fields');
+            die;
+
+        }
+
+        /*
+         * Get data
+         */
+
+        try {
+
+            $group = $this->auth->getGroup($id);
+
+        } catch (InvalidGroupException $e) {
+
+            abort(404, 'Unable to get group: group ID does not exist');
+            die;
+
+        }
+
+        /*
+         * Filter fields
+         */
+
+        if (isset($request['fields']['groups'])) {
+
+            $request = $this->requireValues($request, 'fields.groups', 'id');
+
+            $group = Arr::only($group, $request['fields']['groups']);
+
+        }
+
+        /*
+         * Build schema
+         */
+
+        $schema = GroupResource::create($group, [
+            'object_prefix' => '/groups'
+        ]);
+
+        /*
+         * Send response
+         */
+
+        $this->response->setHeaders([
+            'Cache-Control' => 'max-age=3600' // 1 hour
+        ])->sendJson($schema);
+
+    }
+
+    /**
+     * Get groups.
+     *
+     * @return void
+     *
+     * @throws HttpException
+     * @throws InvalidSchemaException
+     * @throws InvalidStatusCodeException
+     * @throws NotFoundException
+     */
+
+    protected function _getGroups(): void
+    {
+
+        /*
+         * Check permissions
+         */
+
+        if (!$this->hasAnyPermissions([
+            'global.groups.read',
+            'self.groups.read'
+        ])) {
+
+            abort(403, 'Unable to get groups: insufficient permissions');
+            die;
+
+        }
+
+        /*
+         * Get request
+         */
+
+        $request = $this->api->parseQuery(
+            Request::getQuery(),
+            Arr::get(Request::getQuery(), 'page.size', get_config('api.default_page_size', 10)),
+            get_config('api.max_page_size', 100)
+        );
+
+        /*
+         * Validate field types and fields
+         *
+         * Valid fields should match what is available to be
+         * returned in the schema.
+         */
+
+        if (!empty(Arr::except($request['fields'], [ // Valid field types
+                'groups'
+            ])) || !empty(Arr::except(array_flip(Arr::get($request['fields'], 'groups', [])), [ // Valid fields
+                'id',
+                'name',
+                'createdAt',
+                'updatedAt'
+            ]))) {
+
+            abort(400, 'Unable to get groups: query string contains invalid fields');
+            die;
+
+        }
+
+        /*
+         * Filter fields
+         */
+
+        $request = $this->requireValues($request, 'fields.groups', 'id');
+
+        /*
+         * Get data
+         */
+
+        try {
+
+            if (!$this->hasPermissions('global.groups.read')) {
+
+                $groups = $this->auth->getGroupsCollection($request, $this->user_groups); // Limit to user's groups
+
+            } else {
+
+                $groups = $this->auth->getGroupsCollection($request); // Get all roles
+
+            }
+
+        } catch (QueryException|PDOException $e) {
+
+            abort(400, 'Unable to get groups: invalid request');
+            die;
+
+        }
+
+        /*
+         * Build schema
+         */
+
+        $schema = GroupCollection::create($groups, [
+            'object_prefix' => '/groups',
+            'collection_prefix' => '/groups'
+        ]);
+
+        /*
+         * Send response
+         */
+
+        $this->response->setHeaders([
+            'Cache-Control' => 'max-age=3600' // 1 hour
+        ])->sendJson($schema);
+
+    }
+
+    /**
+     * Delete group.
+     *
+     * @param string $id
+     *
+     * @return void
+     *
+     * @throws ChannelNotFoundException
+     * @throws HttpException
+     * @throws InvalidStatusCodeException
+     * @throws NotFoundException
+     */
+
+    protected function _deleteGroup(string $id): void
+    {
+
+        /*
+         * Check permissions
+         */
+
+        if (!$this->hasPermissions('global.groups.delete')) {
+
+            abort(403, 'Unable to delete group: insufficient permissions');
+            die;
+
+        }
+
+        /*
+         * Perform action
+         */
+
+        $deleted = $this->auth->deleteGroup($id);
+
+        if ($deleted) {
+
+            /*
+             * Log action
+             */
+
+            log_info('Group deleted', [
+                'id' => $id
+            ]);
+
+            /*
+             * Do event
+             */
+
+            do_event('group.delete', $id);
+
+            /*
+             * Send response
+             */
+
+            $this->response->setStatusCode(204)->send();
+
+        } else {
+
+            abort(404, 'Unable to delete group: group ID does not exist');
+            die;
+
+        }
+
+    }
+
+    /*
+     * ############################################################
+     * Public methods
+     * ############################################################
+     */
+
+    /**
+     * Router destination.
+     *
+     * @param array $params
+     *
+     * @return void
+     *
+     * @throws ChannelNotFoundException
+     * @throws HttpException
+     * @throws InvalidGroupException
+     * @throws InvalidSchemaException
+     * @throws InvalidStatusCodeException
+     * @throws NotFoundException
+     */
+
+    public function index(array $params)
+    {
+
+        $this->api->allowedMethods([
+            'POST',
+            'GET',
+            'PATCH',
+            'DELETE'
+        ]);
+
+        if (Request::isPost()) {
+
+            if (isset($params['id'])) {
+                abort(400);
+                die;
+            }
+
+            $this->_createGroup();
+
+        } else if (Request::isGet()) {
+
+            if (isset($params['id'])) { // Single group
+
+                $this->_getGroup($params['id']);
+
+            } else { // Get all groups
+
+                $this->_getGroups();
+
+            }
+
+        } else if (Request::isPatch()) {
+
+            if (!isset($params['id'])) {
+                abort(400);
+                die;
+            }
+
+            $this->_updateGroup($params['id']);
+
+        } else { // Delete
+
+            if (!isset($params['id'])) {
+                abort(400);
+                die;
+            }
+
+            $this->_deleteGroup($params['id']);
+
+        }
+
+    }
+
+}
