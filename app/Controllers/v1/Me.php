136b04ab069e7d7859fdbdf2ef7afad6e0e4b3d4
John Robinson <john@bayfrontmedia.com>
2021-02-15 18:12:16 -0500

    Removed file headers

--- app/Controllers/v1/Me.php
+++ app/Controllers/v1/Me.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * @package bones-api
- * @link https://github.com/bayfrontmedia/bones-api
- * @author John Robinson <john@bayfrontmedia.com>
- * @copyright 2021 Bayfront Media
- */
-
 namespace App\Controllers\v1;
 
 use Bayfront\ArraySchema\InvalidSchemaException;
